1ed00124cb03da055711104d09892fe3fee53f44
Arthur <121949966+ShinyWasabi@users.noreply.github.com>
2026-03-18 13:58:34 +0300

    add all tunables_registration globals

--- tunables/GlobalBlock_Tunables.hpp
+++ tunables/GlobalBlock_Tunables.hpp
@@ -1,8 +1,16 @@
 #pragma once
 #include "../Types.hpp"
+#include "Tunables.hpp"
+#include "TunablesGroupModifier.hpp"
 
 struct GLOBAL_BLOCK_TUNABLES
 {
-    scrValue PAD_000000[262144];
+    scrValue RefreshTunables;                      // 262144
+    TUNABLES Tunables;                             // 262145
+    TUNABLES_GROUP_MODIFIER TunablesGroupModifier; // 300131
+    scrValue ShouldRunNetTunableCheck;             // 300178 (determines whether net_tunable_check should run, though this script is unused)
+    scrValue AreTunablesInitialized;               // 300179
+    scrValue AreTunablesInitialized2;              // 300180 (only used once in freemode for something related to car meet prize vehicle)
+    scrValue PAD_038037[224107];
 };
 static_assert(SCR_SIZEOF(GLOBAL_BLOCK_TUNABLES) == SCR_GLOBAL_BLOCK_SIZE);
---
+++ tunables/TunablesGroupModifier.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include "../Types.hpp"
+
+struct TUNABLES_GROUP_MODIFIER
+{
+    scrValue ARMOUR_GROUP_MODIFIER;                 // 0
+    scrValue BEARDS_GROUP_MODIFIER;                 // 1
+    scrValue EXPLOSIVES_AND_HEAVIES_GROUP_MODIFIER; // 2
+    scrValue FACEPAINT_MAKE_UP_GROUP_MODIFIER;      // 3
+    scrValue GUNS_GROUP_MODIFIER;                   // 4
+    scrValue HAIRCUTS_GROUP_MODIFIER;               // 5
+    scrValue MELEE_GROUP_MODIFIER;                  // 6
+    scrValue PARACHUTE_ITEMS_GROUP_MODIFIER;        // 7
+    scrValue TINTS_GROUP_MODIFIER;                  // 8
+    scrValue MALE_LOWERS_GROUP_MODIFIER;            // 9
+    scrValue MALE_UPPERS_GROUP_MODIFIER;            // 10
+    scrValue MALE_SHOES_GROUP_MODIFIER;             // 11
+    scrValue MALE_HATS_GROUP_MODIFIER;              // 12
+    scrValue MALE_HELMETS_GROUP_MODIFIER;           // 13
+    scrValue MALE_GLASSES_GROUP_MODIFIER;           // 14
+    scrValue MALE_WATCHES_GROUP_MODIFIER;           // 15
+    scrValue MALE_MASKS_GROUP_MODIFIER;             // 16
+    scrValue FEMALE_LOWERS_GROUP_MODIFIER;          // 17
+    scrValue FEMALE_UPPERS_GROUP_MODIFIER;          // 18
+    scrValue FEMALE_SHOES_GROUP_MODIFIER;           // 19
+    scrValue FEMALE_HATS_GROUP_MODIFIER;            // 20
+    scrValue FEMALE_HELMETS_GROUP_MODIFIER;         // 21
+    scrValue FEMALE_WATCHES_GROUP_MODIFIER;         // 22
+    scrValue FEMALE_MASKS_GROUP_MODIFIER;           // 23
+    scrValue BRAKES_GROUP_MODIFIER;                 // 24
+    scrValue ENGINE_GROUP_MODIFIER;                 // 25
+    scrValue MUFFLER_GROUP_MODIFIER;                // 26
+    scrValue EXPLOSIVES_GROUP_MODIFIER;             // 27
+    scrValue GRILLE_GROUP_MODIFIER;                 // 28
+    scrValue HOOD_GROUP_MODIFIER;                   // 29
+    scrValue HORN_GROUP_MODIFIER;                   // 30
+    scrValue INSURANCE_GROUP_MODIFIER;              // 31
+    scrValue LIGHTS_GROUP_MODIFIER;                 // 32
+    scrValue PLATES_GROUP_MODIFIER;                 // 33
+    scrValue ROOF_GROUP_MODIFIER;                   // 34
+    scrValue SKIRTS_GROUP_MODIFIER;                 // 35
+    scrValue SPOILER_GROUP_MODIFIER;                // 36
+    scrValue SUSPENSION_GROUP_MODIFIER;             // 37
+    scrValue TYRES_GROUP_MODIFIER;                  // 38
+    scrValue WINDOWS_GROUP_MODIFIER;                // 39
+    scrValue FENDERS_GROUP_MODIFIER;                // 40
+    scrValue LIVERY_GROUP_MODIFIER;                 // 41
+    scrValue WHEEL_MOD_GROUP_MODIFIER;              // 42
+    scrValue PAINT_JOB_GROUP_MODIFIER;              // 43
+    scrValue FEMALE_GLASSES_GROUP_MODIFIER;         // 44
+    scrValue MALE_ACCESSORIES_GROUP_MODIFIER;       // 45
+    scrValue FEMALE_ACCESSORIES_GROUP_MODIFIER;     // 46
+};
+static_assert(SCR_SIZEOF(TUNABLES_GROUP_MODIFIER) == 47);
